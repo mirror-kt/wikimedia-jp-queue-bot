8a1527b5dde03d8129d12b8df8d3b81f2f151d06
mirror-kt <me@mirror-kt.dev>
2023-09-01 22:15:01 +0900

    ✨ Use daily page template to create page

--- src/bin/daily_page_create.rs
+++ src/bin/daily_page_create.rs
@@ -1,8 +1,9 @@
 use anyhow::Context as _;
 use chrono::{Days, FixedOffset, Utc};
-use indoc::formatdoc;
 use mwbot::{Bot, SaveOptions};
 use tracing::info;
+
+const PAGE_TEMPLATE: &str = "プロジェクト:カテゴリ関連/議論/日別ページ雛形";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,24 +19,26 @@ async fn main() -> anyhow::Result<()> {
     let month = tomorrow.format("%-m"); // without 0-padding
     let day = tomorrow.format("%-d"); // without 0-padding
 
+    let page_template = bot
+        .page(PAGE_TEMPLATE)
+        .context("could not get template page")?
+        .wikitext()
+        .await?;
+
     // 翌日分のページを作成
     let page_name = format!("プロジェクト:カテゴリ関連/議論/{year}年/{month}月{day}日");
+
+    let page_content = page_template
+        .replace("{year}", &year.to_string())
+        .replace("{month}", &month.to_string())
+        .replace("{day}", &day.to_string())
+        .replace("{page_name}", &page_name);
 
     let page = bot.page(&page_name)?;
     if page.exists().await? {
         info!("page {} already exists", page.title());
         return Ok(());
     }
-
-    let page_content = formatdoc! {r#"
-        <noinclude>{{{{プロジェクト:カテゴリ関連/議論/見出し|年={year}|月={month}|日={day}}}}}
-
-        __NEWSECTIONLINK__
-
-        == [[{page_name}|カテゴリ]] ==
-        </noinclude><includeonly>== [[{page_name}|{month}月{day}日]] ==</includeonly>
-        <!-- 新規の議論は一番下に記入してください。 -->
-    "#};
 
     page.save(page_content, &SaveOptions::summary("BOT: 議論ページの作成"))
         .await?;
